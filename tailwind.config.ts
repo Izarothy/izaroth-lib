e23f552abe6768bce6da37e531078b3a5c076c2b
Izarothy <izarothmail@gmail.com>
2023-06-21 22:13:38 +0200

    set color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,12 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      'dark': '#040708',
+      'white': '#fefeff',
+      'gray': '#b4ada2',
+      'blue': '#0ea5e9'
+    },
     extend: {},
   },
   plugins: [],
