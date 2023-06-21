65cc5ddf53f2825ace2c18f3a6051f3540ec3f3b
Izarothy <izarothmail@gmail.com>
2023-06-21 22:08:53 +0200

    project setup

---
+++ prettier.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config} */
+const config = {
+  "arrowParens": "avoid",
+  "bracketSpacing": false,
+  "jsxBracketSameLine": false,
+  "jsxSingleQuote": false,
+  "printWidth": 100,
+  "proseWrap": "always",
+  "quoteProps": "as-needed",
+  "semi": true,
+  "singleQuote": true,
+  "tabWidth": 2,
+  "trailingComma": "es5",
+  "useTabs": false,
+  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+};
+
+module.exports = config;
