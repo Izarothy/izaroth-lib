4165bec51519257951a7694ab5e71e66031e6d18
Izarothy <izarothmail@gmail.com>
2023-06-21 22:42:52 +0200

    add a lighter dark color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,14 @@
-import { type Config } from "tailwindcss";
+import {type Config} from 'tailwindcss';
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     colors: {
-      'dark': '#040708',
-      'white': '#fefeff',
-      'gray': '#b4ada2',
-      'blue': '#0ea5e9'
+      dark: '#040708',
+      lighterdark: '#151717',
+      white: '#fefeff',
+      gray: '#b4ada2',
+      blue: '#0ea5e9',
     },
     extend: {},
   },
