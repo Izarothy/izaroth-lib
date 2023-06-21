2772b0b620db40db1fcab222edc5433875619e4f
Izarothy <izarothmail@gmail.com>
2023-06-21 23:02:22 +0200

    home text bold

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const SideBar = () => {
   return (
     <aside className="flex min-h-full flex-col items-center bg-lighterdark py-8 text-white lg:w-1/6">
-      <p className="text-lg ">
+      <p className="text-lg font-bold">
         <Link href="/">Home</Link>
       </p>
     </aside>
