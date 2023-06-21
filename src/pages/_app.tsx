87cbcfcb74eb2c3caa1f185c3c3516501dfee831
Izarothy <izarothmail@gmail.com>
2023-06-21 22:57:30 +0200

    add montserrat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,33 @@ import {type AppType} from 'next/app';
 import {api} from '~/utils/api';
 import '~/styles/globals.css';
 import Layout from '~/components/Layout';
+import {Montserrat} from 'next/font/google';
 
+const montserrat = Montserrat({
+  weight: ['400', '500', '600', '800'],
+  style: ['normal', 'italic'],
+  display: 'swap',
+  subsets: ['latin'],
+});
 const MyApp: AppType<{session: Session | null}> = ({
   Component,
   pageProps: {session, ...pageProps},
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
+    <>
+      <style jsx global>
+        {`
+          html {
+            font-family: ${montserrat.style.fontFamily};
+          }
+        `}
+      </style>
+      <SessionProvider session={session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </>
   );
 };
 
