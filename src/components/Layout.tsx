645749c6777d0ef22d166e38feade7a2170cb189
Izarothy <izarothmail@gmail.com>
2023-06-22 15:10:16 +0200

    add searchprompt with hotkey

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,12 @@
 import React, {type PropsWithChildren} from 'react';
 import SideBar from './SideBar';
+import SearchPrompt from './SearchPrompt';
 
 const Layout = (props: PropsWithChildren) => {
   return (
     <div className="flex min-h-screen text-white">
       <SideBar />
+      <SearchPrompt />
       <main className="w-full bg-dark">{props.children}</main>
     </div>
   );
---
+++ src/components/SearchPrompt.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import React, {type KeyboardEvent, useEffect} from 'react';
+import {Command} from 'cmdk';
+import {useAppSelector} from '~/utils/hooks/useAppSelector';
+import {useDispatch} from 'react-redux';
+import {toggle} from '~/utils/slices/searchPromptSlice';
+const SearchPrompt = () => {
+  const shown = useAppSelector(state => state.searchPrompt.value.shown);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        dispatch(toggle());
+      }
+    };
+
+    /* The only used prop is shared between both types */
+    // @ts-ignore
+    document.addEventListener('keydown', onKeyDown);
+    // @ts-ignore
+    return () => document.removeEventListener('keydown', onKeyDown);
+  }, [dispatch]);
+
+  return (
+    <Command.Dialog
+      open={shown}
+      className="translate-y-[50%]} absolute left-1/2 top-1/4 order-2 w-1/3 translate-x-[-50%] rounded-md border border-gray bg-lighterdark p-4"
+    >
+      <Command.Input
+        className="w-full bg-lighterdark p-1 text-white outline-none"
+        placeholder="Search..."
+      />
+      {/* command group, item, separator */}
+    </Command.Dialog>
+  );
+};
+
+export default SearchPrompt;
